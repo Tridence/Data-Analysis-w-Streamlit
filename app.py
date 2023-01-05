092de04691376add1ba7d85c1c6a3e2105ea5a83
tridence <brianwanjalabarasa@gmail.com>
2023-01-05 10:43:01 +0300

    Added graphical presentation- Indentation

--- app.py
+++ app.py
@@ -107,9 +107,7 @@ with b1:
     ) #barchart Visualising the population of the cities
 with b2:
     st.line_chart(
-        df,
-        x="City",
-        y="population",
+        df, x="City", y="population"
     ) #Line chart visualising the population of the cities
         
 st.markdown("""---""") #Horizontal Separator
